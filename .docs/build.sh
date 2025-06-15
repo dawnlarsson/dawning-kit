061c27303c17dc5c55187406d42e57eb31cd9644
dawnlarsson <dawn@dawn.day>
2025-06-15 10:03:48 +0200

    doc setup

--- .docs/build.sh
+++ .docs/build.sh
@@ -5,8 +5,49 @@
 #
 source ../doc/kit.sh
 
-rm -rf dist
-mkdir -p dist
-cp -r assets/* dist/
+# Replaces <meta template_body> with the content of the second argument file
+template_replace() {
+        local placeholder="$1"
+        local template_file="$2"
+        local content="$3"
+        local replacement_content
+        local template_content
+        local original_template_content
 
-less_css "style/*.css" dist/style.css
+        # Read the entire template file into a variable
+        original_template_content=$(<"$template_file")
+        template_content="$original_template_content"
+
+        # Perform the replacement in the variable
+        template_content="${template_content//$placeholder/$content}"
+
+        # Check if any replacement occurred
+        if [[ "$template_content" != "$original_template_content" ]]; then
+                # Write the modified content back to the original file
+                printf "%s" "$template_content" >"$template_file"
+        else
+                echo "Warning: '$placeholder' not found in $template_file" >&2
+        fi
+}
+
+build() {
+        rm -rf dist
+        mkdir -p dist
+        cp -r assets/* dist/
+
+        less_css "style/*.css" dist/style.css
+
+        cp template.html dist/index.html
+
+        index=$(doc ../README.md)
+        template_replace "<meta template_body>" dist/index.html "$index"
+}
+
+build
+
+if [[ "$1" == "watch" ]]; then
+        while inotifywait -e modify -r .; do
+                build
+                echo "Rebuilt documentation."
+        done
+fi
